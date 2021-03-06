99f4bc5bb8a0b942d2eeebc8c9ce147874e9036e
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2021-03-06 14:15:06 -0500

    satweek2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Row } from 'reactstrap';
 
 class CampsiteInfo extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            
-        };
+        this.state = {};
     };
 
-    render () {
-        return ()
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <div>
+                    {comments.map(comment => <div key={comment.id}> {comment.text} </div>)}
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    renderCampsite(campsite) {
+        console.log(campsite)
+        return (
+            <div className=" col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+
+    render () {        
+        if (this.props.campsite) {
+            return (
+                <Row>                    
+                    <div>{this.renderComments(this.props.campsite.comments)}</div>                    
+                </Row>
+            )
+        }
+        return (
+            <div></div>
+        )
     }
 }
 
